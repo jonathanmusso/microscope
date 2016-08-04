d418822ebf45b143bcffb8e21c969caa3ff1c57d
Jonathan Musso <jonathanmusso@gmail.com>
2016-08-04 07:28:14 -0400

    Passed postId through publish function

--- server/publications.js
+++ server/publications.js
@@ -2,7 +2,7 @@ Meteor.publish('posts', function() {
   return Posts.find();
 });
 
-Meteor.publish('comments', function() {
+Meteor.publish('comments', function(postId) {
   check(postId, String);
   return Comments.find({postId: postId});
 });
