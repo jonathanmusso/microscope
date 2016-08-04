77f341454cc73a209dbad4b016577c76597726de
Jonathan Musso <jonathanmusso@gmail.com>
2016-08-03 23:20:43 -0400

    Made a simple publication/subscription for comments

--- server/publications.js
+++ server/publications.js
@@ -3,5 +3,6 @@ Meteor.publish('posts', function() {
 });
 
 Meteor.publish('comments', function() {
-  return Comments.find();
+  check(postId, String),
+  return Comments.find({postId: postId});
 });
